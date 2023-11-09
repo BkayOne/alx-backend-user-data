e49afe2feaa33754d2303521b29561e8babdf4b8
Frankelly Ozonwu <chiedoziefrankelly@gmail.com>
2023-11-09 02:24:01 +0000

    Quick fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,50 +1,90 @@
 #!/usr/bin/env python3
-"""Session authentication module for the API.
 """
-from uuid import uuid4
-from flask import request
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
 
-from .auth import Auth
-from models.user import User
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+
+# check the AUTH_TYPE
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
-class SessionAuth(Auth):
-    """Session authentication class.
+@app.before_request
+def before_request():
+    """_summary_
+
+    Returns:
+        _type_: _description_
     """
-    user_id_by_session_id = {}
+    if auth is None:
+        pass
+    else:
+        setattr(request, "current_user", auth.current_user(request))
+        excluded_list = ['/api/v1/status/',
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
 
-    def create_session(self, user_id: str = None) -> str:
-        """Creates a session ID for the user.
-        """
-        if isinstance(user_id, str):
-            session_id = str(uuid4())
-            self.user_id_by_session_id[session_id] = user_id
-            return session_id
+        if auth.require_auth(request.path, excluded_list):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description='Forbidden')
 
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Retrieves the user ID of the user associated with
-        a given session ID.
-        """
-        if isinstance(session_id, str):
-            return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None) -> User:
-        """Retrieves the user associated with the request.
-        """
-        user_id = self.user_id_for_session_id(self.session_cookie(request))
-        return User.get(user_id) if user_id else None
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
-    def destroy_session(self, request=None):
-        """Destroys an authenticated session.
-        """
-        session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(session_id)
-        
-        if request is None or session_id is None or user_id is None:
-            return False
-        
-        if session_id in self.user_id_by_session_id:
-            del self.user_id_by_session_id[session_id]
-            return True
-        
-        return False
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """_summary_
+
+    Args:
+        error (_type_): _description_
+
+    Returns:
+        str: _description_
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """_summary_
+
+    Args:
+            error (_type_): _description_
+
+    Returns:
+            str: _description_
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
